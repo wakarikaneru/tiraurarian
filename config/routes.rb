a005d8d68f3235e801f089ec47e013a55f9f713e
wakaru <wakaru@wakaru.orz.hm>
2020-02-21 14:32:37 +0900

    adminモデルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-    resources :admins
-    resources :bads
+      resources :bads
     resources :tweets
     resources :bookmarks
     resources :follows

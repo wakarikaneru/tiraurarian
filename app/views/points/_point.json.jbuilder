5b92af11adf04fd308a8dccd0a6f363b64abc7cc
wakaru <wakaru@wakaru.orz.hm>
2020-02-23 11:35:51 +0900

    ポイントモデルを追加

---
+++ app/views/points/_point.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! point, :id, :user_id, :point, :created_at, :updated_at
+json.url point_url(point, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200219055739) do
+ActiveRecord::Schema.define(version: 20200223021803) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "user_id"
@@ -60,6 +60,24 @@ ActiveRecord::Schema.define(version: 20200219055739) do
     t.datetime "updated_at",      null: false
   end
 
+  create_table "notices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "sender"
+    t.text     "content",         limit: 65535
+    t.boolean  "read_flag"
+    t.datetime "create_datetime"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  create_table "points", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer  "user_id"
+    t.integer  "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "tweet_id"
     t.integer  "user_id"
@@ -91,8 +109,6 @@ ActiveRecord::Schema.define(version: 20200219055739) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
-    t.string   "email",                                default: ""
-    t.string   "login_id",                             default: "", null: false
     t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -100,11 +116,17 @@ ActiveRecord::Schema.define(version: 20200219055739) do
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
     t.string   "name"
+    t.string   "avater_file_name"
+    t.string   "avater_content_type"
+    t.bigint   "avater_file_size"
+    t.datetime "avater_updated_at"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.bigint   "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text     "description",            limit: 65535
+    t.string   "login_id",                             default: "", null: false
+    t.string   "email",                                default: "", null: false
     t.index ["login_id"], name: "index_users_on_login_id", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end

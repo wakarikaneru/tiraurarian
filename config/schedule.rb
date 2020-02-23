650520b88cfdbbe49439f90e106a6c9cc10b6f9d
wakaru <wakaru@wakaru.orz.hm>
2020-02-23 22:29:07 +0900

    スケジュールを修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,10 @@
 
 # Learn more: http://github.com/javan/whenever
 
+set :output, 'log/crontab.log'
+set :environment, :production
+set :runner_command, "rails runner"
+
 every 1.days, at: '23:59' do
   runner "User.collect_points"
 end

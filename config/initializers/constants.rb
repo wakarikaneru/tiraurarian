ea366e1ec37ce00f51beb5af194871703a755df4
wakaru <wakaru@wakaru.orz.hm>
2020-02-28 11:30:16 +0900

    ポイントの供給率を変更

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,10 +2,10 @@ module Constants
   NOTICE_RETENTION_PERIOD = 7.days
   MESSAGE_RETENTION_PERIOD = 30.days
 
-  DISTRIBUTE_RATIO = 0.01
-  DISTRIBUTE_MIN = 10
+  DISTRIBUTE_RATIO = 0.005
+  DISTRIBUTE_MIN = 100
   TAX_RATIO = 0.01
-  TAX_MIN = 10
+  TAX_MIN = 1
 
 
   CARD_ELEMENTS = ["無", "火", "風", "土", "水", "光", "闇"]

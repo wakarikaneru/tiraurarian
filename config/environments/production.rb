29eddc5dfba8d7486d938a6e0695556282683f09
wakaru <wakaru@wakaru.orz.hm>
2020-03-20 08:33:51 +0900

    静的要素をサブドメインに

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_controller.asset_host ="http://static.tirauraria.me"
+  
   config.colorize_logging = false
 end

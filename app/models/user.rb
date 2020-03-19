d6d66846e89a1e300380c897cd4be7b94a228a2a
wakaru <wakaru@wakaru.orz.hm>
2020-03-19 09:31:27 +0900

    rails 5.2 にアップデート

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   before_create :format_description
   before_update :format_description
 

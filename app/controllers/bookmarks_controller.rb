1916d92cf9071ac38c0fdf9318fc8aff0ec9a385
wakaru <wakaru@wakaru.orz.hm>
2020-03-19 10:23:20 +0900

    バグを修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -21,7 +21,7 @@ class BookmarksController < ApplicationController
 
     respond_to do |format|
       if @bookmark.save
-        format.html { redirect_to redirect_back(fallback_location: root_path, notice: "ブックマークに設定しました。" )}
+        format.html { redirect_back(fallback_location: root_path, notice: "ブックマークに設定しました。" )}
         format.json { render :show, status: :created, location: @bookmark }
       else
         format.html { render :new }
@@ -36,12 +36,12 @@ class BookmarksController < ApplicationController
     if @bookmark.user_id == current_user.id
       @bookmark.destroy
       respond_to do |format|
-        format.html { redirect_to redirect_back(fallback_location: root_path, notice: "ブックマークを解除しました。" )}
+        format.html { redirect_back(fallback_location: root_path, notice: "ブックマークを解除しました。" )}
         format.json { head :no_content }
       end
     else
       respond_to do |format|
-        format.html { redirect_to redirect_back(fallback_location: root_path, alert: "権限がありません。" )}
+        format.html { redirect_back(fallback_location: root_path, alert: "権限がありません。" )}
         format.json { head :no_content }
       end
     end

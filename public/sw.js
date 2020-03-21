1cb29ca876e6063a3aaeb4e374299815cb2b62bd
wakaru <wakaru@wakaru.orz.hm>
2020-03-21 22:34:50 +0900

    fix

--- public/sw.js
+++ public/sw.js
@@ -2,9 +2,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).then((response) => {
       cache.put(event.request, response.clone());
-      return response;
-    }).catch(() => {
-      return caches.match(event.request);
+      return response || caches.match(event.request);
     })
   );
 });

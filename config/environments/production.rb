283641c2c8343969b8282eb7a8e018b84ee6e01b
wakaru <wakaru@wakaru.orz.hm>
2020-03-21 14:51:46 +0900

    ログ設定をもとに戻した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.logger = Logger.new("log/production.log", 'daily')
 
   # Code is not reloaded between requests.
   config.cache_classes = true

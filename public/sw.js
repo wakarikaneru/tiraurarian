cf24276ae2efc57cad96bbc63ec3facdeecddbdc
wakaru <wakaru@wakaru.orz.hm>
2020-03-21 22:44:51 +0900

    fix

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).then((response) => {
-      caches.put(event.request, response.clone());
-      return response || caches.match(event.request);
+    caches.open('tirauraria.me').then((cache) => {
+      fetch(event.request).then((response) => {
+        cache.put(event.request, response.clone());
+        return response || cache.match(event.request);
+      })
     })
   );
 });

62c2c3a589dd55dfeb1edbe2c933040c60f4f369
wakaru <wakaru@wakaru.orz.hm>
2020-03-22 11:06:57 +0900

    fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,6 @@ console.log('Hello from service-worker.js');
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.open('tirauraria-v1').then((cache) => {
-      event.updateUI({ title: 'download finished' });
       return fetch(event.request).then((response) => {
         cache.put(event.request, response.clone());
         return response;

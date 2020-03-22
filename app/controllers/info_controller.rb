b6a1c63d099092cfb53d8cd09a045bb3a4e34e87
wakaru <wakaru@wakaru.orz.hm>
2020-03-22 13:42:47 +0900

    オフライン用ページを追加

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -13,4 +13,7 @@ class InfoController < ApplicationController
 
   def whatisvarth
   end
+
+  def offline
+  end
 end
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,20 @@
+CACHE_NAME = "tirauraria-v1"
+
 console.log('Hello from service-worker.js');
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/info/offline'
+      ]);
+    })
+  );
+});
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.open('tirauraria-v1').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return fetch(event.request).then((response) => {
         cache.put(event.request, response.clone());
         return response;

428a941621abf82c7c7ec434586d638b96c66227
wakaru <wakaru@wakaru.orz.hm>
2020-04-10 23:43:04 +0900

    未読表示をFix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def notification_counts
+    if user_signed_in? then
+      notice_records = Notice.where(user_id: current_user.id, read_flag: false).where("create_datetime > ?", Constants::NOTICE_RETENTION_PERIOD.ago)
+      message_records = Message.where(user_id: current_user.id, read_flag: false).where("create_datetime > ?", Constants::MESSAGE_RETENTION_PERIOD.ago)
+
+      my_tweets = Tweet.where(user_id: current_user.id)
+      my_tweets_res = Tweet.where(id: (current_user.last_check_res + 1)..Float::INFINITY).where(parent_id: my_tweets).where.not(user_id: current_user.id)
+      res_count = my_tweets_res.count
+    else
+      notice_records = Notice.none
+      message_records = Message.none
+      res_count = 0
+    end
+
+    notice_count = notice_records.count
+    message_count = message_records.count
+    total_count = notice_count + message_count
+
+    notification = {datetime: Time.current.to_s, res: res_count, notice: notice_count, message: message_count, total: total_count}
+    return notification
+  end
+
 end

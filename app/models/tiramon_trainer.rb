6aa21850d111297c0e942b3c3d820aab2193aea0
wakaru <wakaru.orz.hm@gmail.com>
2021-02-23 22:47:35 +0900

    行動力の回復

--- app/models/tiramon_trainer.rb
+++ app/models/tiramon_trainer.rb
@@ -19,4 +19,19 @@ class TiramonTrainer < ApplicationRecord
     end
   end
 
+  def self.recovery
+    all_user = User.all
+    all_user.find_each do |user|
+      TiramonTrainer.find_or_create_by(user_id: user.id)
+    end
+
+    all_trainer = TiramonTrainer.all
+    all_trainer.find_each do |trainer|
+      trainer.move = 3 + (trainer.level / 10)
+      trainer.tiramon_ball = trainer.tiramon_ball + 1
+      trainer.save!
+      Notice.generate(trainer.user_id, 0, "チラモン闘技場", "行動ポイントが回復しました。")
+    end
+  end
+
 end

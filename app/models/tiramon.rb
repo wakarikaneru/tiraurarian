e2721efdbfb0df7d1244bf71c286626a440c1f6a
wakaru <wakaru.orz.hm@gmail.com>
2021-02-25 14:37:31 +0900

    チラモンの調整

--- app/models/tiramon.rb
+++ app/models/tiramon.rb
@@ -609,7 +609,7 @@ class Tiramon < ApplicationRecord
         case training_id
         when 0 then
           v = d[:weight]
-          e = [(d[:height] ** 2 * 38) - v, 0.0].max * (1 + Tiramon.dist_rand(2)) * 0.20
+          e = [(d[:height] ** 2 * 40) - v, 0.0].max * (1 + Tiramon.dist_rand(2)) * 0.20
           d[:weight] += e
           amount = e.abs
 

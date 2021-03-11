cf405d0966ce45bfa00621400233fb71f0c54ba1
wakaru <wakaru.orz.hm@gmail.com>
2021-03-11 11:07:52 +0900

    株調整

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -77,12 +77,12 @@ class Stock < ApplicationRecord
     appearance_economy_f = appearance_economy.value.to_f
 
     if (Random.rand * ((60.0 / Constants::STOCK_UPDATE_SECOND.to_f) * 60 * 12)) < 1
-      economy_f = dist_rand(1) * 20.0
-      appearance_economy_f = dist_rand(1) * 20.0
+      economy_f = dist_rand(1) * 200.0
+      appearance_economy_f = dist_rand(1) * 200.0
     else
-      economy_f = economy_f + (dist_rand(3) * 10.0) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
+      economy_f = economy_f + (dist_rand(2) * 10.0) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
       economy_f = economy_f - (economy_f * 0.05) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
-      appearance_economy_f = appearance_economy_f + (dist_rand(3) * 10.0) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
+      appearance_economy_f = appearance_economy_f + (dist_rand(2) * 10.0) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
       appearance_economy_f = appearance_economy_f - (appearance_economy_f * 0.05) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
     end
 
@@ -95,7 +95,7 @@ class Stock < ApplicationRecord
     price_target = Control.find_or_create_by(key: "stock_price_target")
     price_target_f = price_target.value.to_f
 
-    price_target_f = price_target_f + (economy_f * coefficient_f * 0.1)
+    price_target_f = price_target_f + (economy_f * coefficient_f * 0.1) * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)
     price_target.update(value: price_target_f.to_s)
 
     price_f = price_f + (economy_f * coefficient_f) * 1.0 * (Constants::STOCK_UPDATE_SECOND.to_f / 60.0)

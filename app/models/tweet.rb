8f1f53dc002ce832df43393cfeada57675b941f1
wakaru <wakaru.orz.hm@gmail.com>
2021-11-21 09:49:13 +0900

    typo修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -14,7 +14,7 @@ class Tweet < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :tags, dependent: :destroy
 
-  validates :content, {presence: true, lengh: {maximum: 140}}
+  validates :content, {presence: true, length: {maximum: 140}}
 
   accepts_nested_attributes_for :text
 

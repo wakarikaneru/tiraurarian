9dc83082e720432aadfd509c704c497d8f2987d0
wakaru <wakaru.orz.hm@gmail.com>
2022-01-30 20:30:11 +0900

    攻撃対策テスト

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,10 +1,14 @@
 class Rack::Attack
 
-  throttle('req/ip', :limit => 15, :period => 1.second) do |req|
+  throttle('req/ip short', :limit => 15, :period => 1.second) do |req|
     req.ip # unless req.path.start_with?('/assets')
   end
 
-  throttle('req/ip', :limit => 360, :period => 3.minutes) do |req|
+  throttle('req/ip mid', :limit => 360, :period => 3.minutes) do |req|
     req.ip # unless req.path.start_with?('/assets')
   end
+
+  throttle('request dist', :limit => 15000, :period => 10.minutes) do |req|
+  end
+
 end
